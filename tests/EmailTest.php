fd987d096c0b1945ee83eb975d6a4e6640f19817
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-03-18 21:00:35 +0000

    Add CI files

---
+++ tests/EmailTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+use GuiBranco\Pancake\Email;
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmail(): void
+    {
+        $string = 'user@example.com';
+
+        $email = Email::fromString($string);
+
+        $this->assertSame($string, $email->asString());
+    }
+
+    public function testCannotBeCreatedFromInvalidEmail(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+}
