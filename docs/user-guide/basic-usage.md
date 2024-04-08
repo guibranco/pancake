bacb7a01e07bd1ce5c8d7978c505b516aef56bc3
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-04-08 22:49:08 +0100

    Add HealthChecks class +semver: minor

--- docs/user-guide/basic-usage.md
+++ docs/user-guide/basic-usage.md
@@ -4,4 +4,5 @@ Main namespace is `GuiBranco\Pancake`
 
 ## Features
 
+- [Health Checks](health-checks.md)
 - [HTTP Requests](request.md)
---
+++ tests/HealthChecksTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GuiBranco\Pancake\Tests;
+
+use GuiBranco\Pancake\HealthChecks;
+use PHPUnit\Framework\TestCase;
+
+final class HealthChecksTest extends TestCase
+{
+    public function testCanCreateHealthCheck(): void
+    {
+        $healthCheck = new HealthChecks('token');
+        $this->assertInstanceOf(HealthChecks::class, $healthCheck);
+    }
+
+    public function testCanCreateHealthCheckWithRid(): void
+    {
+        $healthCheck = new HealthChecks('token', 'rid');
+        $this->assertInstanceOf(HealthChecks::class, $healthCheck);
+    }
+
+    public function testCanSetHeaders(): void
+    {
+        $healthCheck = new HealthChecks('token');
+        $healthCheck->setHeaders(['key' => 'value']);
+        $this->assertObjectHasProperty('headers', $healthCheck);
+        $this->assertObjectHasProperty('headersSet', $healthCheck);
+    }
+
+    public function testCanStart(): void
+    {
+        $healthCheck = new HealthChecks('token');
+        $response = $healthCheck->start();
+        $this->assertInstanceOf(\stdClass::class, $response);
+    }
+
+    public function testCanFail(): void
+    {
+        $healthCheck = new HealthChecks('token');
+        $response = $healthCheck->fail();
+        $this->assertInstanceOf(\stdClass::class, $response);
+    }
+
+    public function testCanEnd(): void
+    {
+        $healthCheck = new HealthChecks('token');
+        $response = $healthCheck->end();
+        $this->assertInstanceOf(\stdClass::class, $response);
+    }
+
+    public function testCanError(): void
+    {
+        $healthCheck = new HealthChecks('token');
+        $response = $healthCheck->error('error message');
+        $this->assertInstanceOf(\stdClass::class, $response);
+    }
+
+    public function testCanResetState(): void
+    {
+        $healthCheck = new HealthChecks('token');
+        $healthCheck->resetState();
+        $this->assertObjectHasProperty('failed', $healthCheck);
+    }
+
+    public function testCanSetHeadersAndFailAndThenEnd(): void
+    {
+        $healthCheck = new HealthChecks('token');
+        $healthCheck->setHeaders(['key' => 'value']);
+        $response = $healthCheck->start();
+        $this->assertInstanceOf(\stdClass::class, $response);
+        $response = $healthCheck->fail();
+        $this->assertInstanceOf(\stdClass::class, $response);
+        $response = $healthCheck->end();
+        $this->assertInstanceOf(\stdClass::class, $response);
+    }
+}
