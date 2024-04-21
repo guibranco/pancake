82faf44f1d391d28dd8ceefb0981d21fd69107fd
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-04-21 15:53:11 +0100

    Add GUID v4 class +semver: minor (#116)
    * Add GUID v4 class +semver: minor
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in d55a120 according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/pancake/pull/116
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ src/GUIDv4.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GuiBranco\Pancake;
+
+class GUIDv4
+{
+    private const EMPTY = '00000000-0000-0000-8000-000000000000';
+
+    public static function empty(): string
+    {
+        return self::EMPTY;
+    }
+
+    public static function random(): string
+    {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            random_int(0, 0xffff),
+            random_int(0, 0xffff),
+            random_int(0, 0xffff),
+            random_int(0, 0x0fff) | 0x4000,
+            random_int(0, 0x3fff) | 0x8000,
+            random_int(0, 0xffff),
+            random_int(0, 0xffff),
+            random_int(0, 0xffff)
+        );
+    }
+}
---
+++ tests/GUIDv4Test.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GuiBranco\Pancake\Tests;
+
+use GuiBranco\Pancake\GUIDv4;
+use PHPUnit\Framework\TestCase;
+
+final class GUIDv4Test extends TestCase
+{
+    public function testCanBeEmptyGUID(): void
+    {
+        $this->assertSame('00000000-0000-0000-8000-000000000000', GUIDv4::empty());
+    }
+
+    public function testCanBeRandomGUID(): void
+    {
+        $guid = GUIDv4::random();
+        $this->assertMatchesRegularExpression('/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/', $guid);
+    }
+}
