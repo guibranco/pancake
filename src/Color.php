43c1eb289494493e56e1339723ba8a99d1464c6a
Gabriel Goulart <116896794+gvieiragoulart@users.noreply.github.com>
2024-07-04 06:18:15 -0300

    Color class (#149)
    * color class
    
    * tests
    
    * documentation
    
    * validate inputs on constructor

---
+++ src/Color.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace GuiBranco\Pancake;
+
+use InvalidArgumentException;
+
+class Color
+{
+
+    private string $red;
+    private string $green;
+    private string $blue;
+    private string $hexColor;
+
+    /**
+     * Constructor.
+     *
+     * @param string $red      The red value.
+     * @param string $green    The green value.
+     * @param string $blue     The blue value.
+     * @param string $hexColor The hex color.
+     */
+    public function __construct(string $red = "", string $green = "", string $blue = "", string $hexColor = "")
+    {
+        if (!empty($red)) {
+            $this->setRed($red);
+        }
+        if (!empty($green)) {
+            $this->setGreen($green);
+        }
+        if (!empty($blue)) {
+            $this->setBlue($blue);
+        }
+        if (!empty($hexColor)) {
+            $this->setHexColor($hexColor);
+        }
+    }
+
+    /**
+     * Set the red value.
+     * 
+     * @param string $hexColor The hex color.
+     */
+    public function setRed(string $red): void
+    {
+        self::validateColor($red);
+        $this->red = $red;
+    }
+
+    public function getRed(): string
+    {
+        return $this->red;
+    }
+
+    /**
+     * Set the green value.
+     * 
+     * @param string $green The green value.
+     */
+    public function setGreen(string $green): void
+    {
+        self::validateColor($green);
+        $this->green = $green;
+    }
+
+    public function getGreen(): string
+    {
+        return $this->green;
+    }
+
+    /**
+     * Set the blue value.
+     * 
+     * @param string $blue The blue value.
+     */
+    public function setBlue(string $blue): void
+    {
+        self::validateColor($blue);
+        $this->blue = $blue;
+    }
+
+    public function getBlue(): string
+    {
+        return $this->blue;
+    }
+
+    /**
+     * Set the hex color.
+     * 
+     * @param string $hexColor The hex color.
+     */
+    public function setHexColor(string $hexColor): void
+    {
+        self::validateHexColor($hexColor);
+        $this->hexColor = $hexColor;
+    }
+
+    public function getHexColor(): string
+    {
+        return $this->hexColor;
+    }
+
+    /**
+     * Validate a color.
+     * 
+     * @param  string $color The color to validate.
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    private static function validateColor(string $color): void
+    {
+        if (!is_numeric($color) || $color < 0 || $color > 255) {
+            throw new InvalidArgumentException("$color is not a valid color");
+        }
+    }
+
+    /**
+     * Validate a hex color.
+     * 
+     * @param  string $hexColor The hex color to validate.
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    private static function validateHexColor(string $hexColor): void
+    {
+        if (!preg_match("/^#[a-f0-9]{6}$/i", $hexColor)) {
+            throw new InvalidArgumentException("$hexColor is not a valid hex color");
+        }
+    }
+
+    /**
+     * Calculate the luminance of a color.
+     * 
+     * @return float      0-1
+     */
+    public function luminanceRGB(): float
+    {
+        return ($this->red * 0.2126 + $this->green * 0.7152 + $this->blue * 0.0722) / 255;
+    }
+
+
+    /**
+     * Convert a hex color to RGB.
+     * 
+     * @return array{int,int,int} [186, 218, 85]
+     */
+    public function hexToRGB(): array
+    {
+        return sscanf($this->hexColor, "#%02x%02x%02x");
+    }
+
+    /**
+     * Generate a color from a text.
+     * 
+     * @param  string $text          The text to generate the color from.
+     * @param  int    $minBrightness The minimum brightness of the color.
+     * @param  int    $spec          The spec of the color.
+     * @return Color                 The generated color.
+     * @throws InvalidArgumentException
+     */
+    public function generateColorFromText(string $text, int $minBrightness = 100, int $spec = 10): Color
+    {
+        if ($spec < 2 || $spec > 10) {
+            throw new InvalidArgumentException("$spec is out of range");
+        }
+        if ($minBrightness < 0 || $minBrightness > 255) {
+            throw new InvalidArgumentException("$minBrightness is out of range");
+        }
+    
+        $hash = md5($text);
+        $colors = array();
+        for ($i = 0; $i < 3; $i++) {
+            $current = round(((hexdec(substr($hash, $spec * $i, $spec))) / hexdec(str_pad("", $spec, "F"))) * 255);
+            $colors[$i] = max(array($current, $minBrightness));
+        }
+     
+        if ($minBrightness > 0) {
+            while (array_sum($colors) / 3 < $minBrightness) {
+                for ($i = 0; $i < 3; $i++) {
+                    $colors[$i] += 10;
+                }
+            }
+        }
+    
+        $output = "";
+    
+        for ($i = 0; $i < 3; $i++) {
+            $output .= str_pad(dechex($colors[$i]), 2, 0, STR_PAD_LEFT);
+        }
+        
+        return new Color(
+            hexColor: "#" . $output
+        );
+    }
+
+
+}
---
+++ tests/ColorTest.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GuiBranco\Pancake\Tests;
+
+use GuiBranco\Pancake\Color;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use ReflectionMethod;
+use Throwable;
+
+final class ColorTest extends TestCase {
+
+    public static function colorRGBProvider(): array 
+    {
+        return [
+            [["red" => 255, "green" => 87, "blue" => 51], 0.4710494117647058],
+            [["red" => 0, "green" => 0, "blue" => 0], 0],
+            [["red" => 255, "green" => 255, "blue" => 255], 0.9999999999999999],
+            [["red" => 255, "green" => 255, "blue" => 0], 0.9277999999999998],
+            [["red" => 0, "green" => 255, "blue" => 0], 0.7152],
+            [["red" => 0, "green" => 0, "blue" => 255], 0.0722],
+        ];
+    }
+
+    public static function hexProvider(): array
+    {
+        return [
+            ["#BADA55", [186, 218, 85]],
+            ["#000000", [0, 0, 0]],
+            ["#FFFFFF", [255, 255, 255]],
+            ["#FFFF00", [255, 255, 0]],
+            ["#00FF00", [0, 255, 0]],
+            ["#0000FF", [0, 0, 255]],
+        ];
+    }
+
+    public static function invalidColorTextProvider(): array
+    {
+        return [
+            [["text" => "blue", "minBrightness" => 256, "spec" => 5]], // Invalid maxBrightness
+            [["text" => "green", "minBrightness" => -10, "spec" => 7]], // Invalid minBrightness
+            [["text" => "yellow", "minBrightness" => 150, "spec" => 1]], // Invalid spec
+            [["text" => "orange", "minBrightness" => 120, "spec" => 11]], // Invalid spec
+        ];
+    }
+
+    public static function validColorTextProvider(): array
+    {
+        return [
+            [["text" => "red", "minBrightness" => 100, "spec" => 10], "#bd6464"],
+            [["text" => "purple", "minBrightness" => 100, "spec" => 5], "#bbdf64"],
+            [["text" => "pink", "minBrightness" => 150, "spec" => 8], "#96dc96"],
+            [["text" => "brown", "minBrightness" => 80, "spec" => 3], "#7050f9"],
+            [["text" => "gray", "minBrightness" => 200, "spec" => 6], "#cdc8c8"],
+        ];
+    }
+
+    #[DataProvider('colorRGBProvider')]
+    public function testLuminance($rgbColor, $expectedLuminance): void 
+    {
+        $color = new Color();
+        $color->setRed((string) $rgbColor["red"]);
+        $color->setGreen((string) $rgbColor["green"]);
+        $color->setBlue((string) $rgbColor["blue"]);
+
+        $this->assertEquals($expectedLuminance, $color->luminanceRGB());
+    }
+
+
+    #[DataProvider('hexProvider')]
+    public function testHexProvider($hexColor, $expectedRGB): void 
+    {
+        $color = new Color();
+        $color->setHexColor($hexColor);
+
+        $this->assertEquals($expectedRGB, $color->hexToRGB());
+    }
+
+    #[DataProvider('validColorTextProvider')]
+    public function testGenerateColorFromTextWithValidProvider($textColor, $expectedHexColor): void 
+    {
+        $color = new Color();
+
+        $this->assertEquals($expectedHexColor, $color->generateColorFromText(
+            text: $textColor["text"],
+            minBrightness: $textColor["minBrightness"],
+            spec: $textColor["spec"],
+        )->getHexColor());
+    }
+
+    #[DataProvider("invalidColorTextProvider")]
+    public function testGenerateColorFromTextWithInvalidProvider($textColor): void 
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $color = new Color();
+        $color->generateColorFromText(
+            text: $textColor["text"],
+            minBrightness: $textColor["minBrightness"],
+            spec: $textColor["spec"],
+        );
+    }
+
+    public function testValidateColorWithValidColor(): void
+    {
+        $color = new Color();
+        $this->expectNotToPerformAssertions();
+
+        try {
+            $reflectionMethod = new ReflectionMethod(Color::class, 'validateColor');
+            $reflectionMethod->setAccessible(true);
+            $reflectionMethod->invoke($color, '100');
+        } catch (Throwable $e) {
+            $this->fail('Exception should not be thrown');
+        }
+    }
+
+    public function testValidateColorWithInvalidColor(): void
+    {
+        $color = new Color();
+        $this->expectException(InvalidArgumentException::class);
+
+        $reflectionMethod = new ReflectionMethod(Color::class, 'validateColor');
+        $reflectionMethod->setAccessible(true);
+        $reflectionMethod->invoke($color, '-10');
+    }
+
+    public function testValidateHexColorWithValidHexColor(): void
+    {
+        $color = new Color();
+        $this->expectNotToPerformAssertions();
+
+        try {
+            $reflectionMethod = new ReflectionMethod(Color::class, 'validateHexColor');
+            $reflectionMethod->setAccessible(true);
+            $reflectionMethod->invoke($color, '#FF0000');
+        } catch (Throwable $e) {
+            $this->fail('Exception should not be thrown');
+        }
+    }
+
+    public function testValidateHexColorWithInvalidHexColor(): void
+    {
+        $color = new Color();
+        $this->expectException(InvalidArgumentException::class);
+
+        $reflectionMethod = new ReflectionMethod(Color::class, 'validateHexColor');
+        $reflectionMethod->setAccessible(true);
+        $reflectionMethod->invoke($color, '#ZZZZZZ');
+    }
+
+}    
