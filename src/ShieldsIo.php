a5735f41b4eae001f40e30cf66dc0eee265bf4d1
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-28 23:56:37 +0100

    Update ShieldsIo.php

--- src/ShieldsIo.php
+++ src/ShieldsIo.php
@@ -6,10 +6,6 @@ class ShieldsIo
 {
     private function encodeShieldsIoParameters($input)
     {
-        if (empty($input)) {
-            return $input;
-        }
-
         $input = str_replace("_", "__", $input);
         $input = str_replace(" ", "_", $input);
         $input = str_replace("-", "--", $input);
@@ -19,36 +15,32 @@ class ShieldsIo
         return $input;
     }
 
+    private function addComponent($component, &$badge)
+    {
+        if (isset($component) && (!empty($component) || $component === 0)) {
+            $value = $this->encodeShieldsIoParameters($component);
+            $badge[] = $value;
+        }
+    }
+
+    private function addQueryString($component, $key, &$queryString)
+    {
+        if (isset($component) && !empty($component)) {
+            $queryString[$key] = $component;
+        }
+    }
+
     public function generateBadgeUrl($label, $content, $color, $style, $labelColor, $logo)
     {
         $badge = array();
-
-        if (isset($label) && $label !== null) {
-            $label = $this->encodeShieldsIoParameters($label);
-            $badge[] = $label;
-        }
-
-        if (isset($content) && $content !== null) {
-            $content = $this->encodeShieldsIoParameters($content);
-            $badge[] = $content;
-        }
-
-        if (isset($color) && $color !== null) {
-            $badge[] = $color;
-        }
+        $this->addComponent($label, $badge);
+        $this->addComponent($content, $badge);
+        $this->addComponent($color, $badge);
 
         $queryString = array();
-        if (isset($style) && $style !== null) {
-            $queryString["style"] = $style;
-        }
-
-        if (isset($labelColor) && $labelColor !== null) {
-            $queryString["labelColor"] = $labelColor;
-        }
-
-        if (isset($logo) && $logo !== null) {
-            $queryString["logo"] = $logo;
-        }
+        $this->addQueryString($style, "style", $queryString);
+        $this->addQueryString($labelColor, "labelColor", $queryString);
+        $this->addQueryString($logo, "logo", $queryString);
 
         return "https://img.shields.io/badge/" . implode("-", $badge) . "?" . http_build_query($queryString);
     }
