472011ba8e208dafdc2cd935ab6615ad4cc3a533
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-02 19:31:02 +0100

    Create Constants.php

---
+++ src/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GuiBranco\Pancake;
+
+class Constants
+{
+  
+  const USER_AGENT_VENDOR = "Pancake/0.11 (+https://github.com/guibranco/pancake)";
+
+  const USER_AGENT_HEADER = "User-Agent: Pancake/0.11 (+https://github.com/guibranco/pancake)";
+
+  const CONTENT_TYPE_JSON_HEADER = "Content-Type: application/json; charset=utf-8";
+}
