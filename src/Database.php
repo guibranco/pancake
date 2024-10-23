aaae84ec05ba2f08c3c6b72c637a5343689634eb
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-23 19:26:56 +0100

    Update Database.php

--- src/Database.php
+++ src/Database.php
@@ -16,9 +16,10 @@ class Database implements IDatabase
         string $dbname,
         string $username,
         string $password,
+        int $port = 3306,
         string $charset = 'utf8mb4'
     ) {
-        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+        $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
