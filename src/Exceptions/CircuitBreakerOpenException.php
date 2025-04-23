3c0df80e9373f422cb6ee04d748b6c31c5bdf613
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-04-23 10:40:00 +0100

    Update CircuitBreakerOpenException.php

--- src/Exceptions/CircuitBreakerOpenException.php
+++ src/Exceptions/CircuitBreakerOpenException.php
@@ -4,6 +4,13 @@ namespace App\Exceptions;
 
 use Exception;
 
+/**
+ * Class CircuitBreakerOpenException
+ *
+ * Exception thrown when a circuit is open and an operation is attempted.
+ * This is part of the Circuit Breaker pattern and indicates that further attempts
+ * should be deferred until the circuit closes or moves to a half-open state.
+ */
 class CircuitBreakerOpenException extends Exception
 {
 }
