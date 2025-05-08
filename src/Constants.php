4fd356105ee42d7a89d8a794c29730f59df66619
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-05-08 13:51:24 +0100

    Update Constants.php

--- src/Constants.php
+++ src/Constants.php
@@ -4,7 +4,7 @@ namespace GuiBranco\Pancake;
 
 class Constants
 {
-    public const USER_AGENT_VENDOR = "Pancake/0.11 (+https://github.com/guibranco/pancake)";
+    public const USER_AGENT_VENDOR = "Pancake/1.3 (+https://github.com/guibranco/pancake)";
 
     public const USER_AGENT_HEADER = "User-Agent: Pancake/1.3 (+https://github.com/guibranco/pancake)";
 
